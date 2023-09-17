eaa7a365437c454a656bf73755f0e0edd8d77521
Maryam Najiarani <mnajiarani@gmail.com>
2023-09-17 15:42:03 -0700

    added js, html, created sqlite file, jupyter of tested sqlite, created final scv all the connection has been made.

---
+++ Crimes.sql
@@ -0,0 +1,19 @@
+-- DROP TABLE IF EXISTS crimes; 
+
+-- Create crimes table 
+CREATE TABLE Crimes(
+    "id"  INTEGER PRIMARY KEY,
+    "year" INTEGER NOT NULL, 
+    "county" VARCHAR(100) NOT NULL, 
+    "month" INTEGER NOT NULL,
+    "county_pop" INTEGER NOT NULL,
+    "violent" INTEGER NOT NULL,
+    "homicide" INTEGER NOT NULL,
+    "rape" INTEGER NOT NULL,
+    "robbery" INTEGER NOT NULL,
+    "agg_assault" INTEGER NOT NULL,
+    "property" INTEGER NOT NULL, 
+    "burglary" INTEGER NOT NULL,
+    "vehicle_theft" INTEGER NOT NULL, 
+    "larceny_theft" INTEGER NOT NULL
+ );
