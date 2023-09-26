ea21ebf7c11b40d3e33b5ab90f774a37b3cc4a9d
JayashreeKotte <jayashree.kotte@gmail.com>
2023-09-25 18:55:24 -0700

    Added visualizations, edited css file and html files

--- static/js/app.js
+++ static/js/app.js
@@ -31,12 +31,13 @@ function init() {
       for (let county of uniqueCountiesSet){
         dropdownMenuCounty.append("option").text(county).property("value", county);
       }
-
-
+    // Calling the createBar function so it displays the graphs for 2019, Alameda County
+    createBar(uniqueCountiesSet[0]);
     });
 
-    
   }
+
+// Create a function to calculate the values for plotting visualizations
 function createBar(){
   
   let dropdownMenuYear = d3.select("#selYear");
@@ -65,12 +66,177 @@ function createBar(){
           crimeDict[crime] += item[crime] ;
         }
       }
-    //log the dictionary object to see the output 
+    // log the dictionary object to see the output 
     console.log("Total of each crime : ", crimeDict);
+
+    // Calling the chart functions to pass crimeDict as an argument for further manipulation
+    createBarChart(crimeDict);
+    createPieChart(crimeDict);
+    
   });
 
+  // Create a bar chart function to plot crime count vs name for user selected year and county 
+  function createBarChart(barChartDict){
 
+    // Extracting keys and values from the dict to convert it into a two dimensional array 
+    var items = Object.keys(barChartDict).map(function(key) {
+      return [key, barChartDict[key]];
+    });
+    
+    // Sorting the two dimensional array in descending order 
+    items.sort(function(first, second) {
+      return second[1] - first[1];
+    });
 
-}
+    // Extracting the first array from the two dimensional array and assigning it to a variable (sorted keys from crimeDict)
+    let xValues = items.map(d => d[0]);
+    console.log("X values");
+    console.log(xValues);
+
+    // Extracting the second array from the two dimensional array and assigning it to a variable (sorted values from crimeDict)
+    let yValues = items.map(d => d[1]);
+    console.log("Y values");
+    console.log(yValues);
+
+    // Creating the bar chart with the arrays stored in the above variables (sorted keys and values from crimeDict)
+    let trace1 = {
+      x: xValues,
+      y: yValues,
+      type: "bar",
+      // This allows modification of the standard hover text in the chart
+      hoverlabel: {
+        bgcolor: 'black',
+        bordercolor: 'black',
+        font: {
+          family: 'Lato',
+          color: 'white',
+          size: 16
+        }
+      },
+
+    };
+
+    let data = [trace1];
+
+    let layout = {
+      title: {
+        text: 'Aggregate Crime Statistics by Year and County ',
+        // This allows modification of the standard title font and size
+        font: {
+          size: 22,
+          family: 'Arial',
+          color: 'black'
+        }
+      },
+      margin: { t: 40, l: 35 }
+    };
+
+    Plotly.newPlot("bar", data, layout);
+
+  };
+
+  // Create a pie chart function to plot aggregate crime rate by year and county
+  function createPieChart(chartDict){
+
+    // Extracting keys and values from the dict to convert it into a two dimensional array
+    var items1 = Object.keys(chartDict).map(function(key) {
+      return [key, chartDict[key]];
+    });
+
+    // Sorting the two dimensional array in descending order
+    items1.sort(function(first, second) {
+      return second[1] - first[1];
+    });
+
+    // Extracting the first array from the two dimensional array and assigning it to a variable (sorted keys from crimeDict)
+    let xVals = items1.map(d => d[0]);
+
+    // Extracting the second array from the two dimensional array and assigning it to a variable (sorted values from crimeDict)
+    let yVals = items1.map(d => d[1]);
+
+    // Initialize aggregate sum and aggregate crime rates to a variable and empty array
+    let aggSum = 0;
+    let aggYVals = [];
+
+    // Calculate the sum of the crime counts in the array
+    for (let i=0; i<yVals.length; i++) {
+        aggSum += yVals[i];
+    };
+
+    // Calculate the average of the crime counts and push it to aggYVals array
+    for (let j=0; j<yVals.length; j++) {
+      console.log(yVals[j])
+      aggYVals.push(((yVals[j]/aggSum)*100).toFixed(2));
+    };
+    console.log(aggSum, aggYVals);
+
+    // Use the above arrays to create a donut chart with crime rate by year and county
+    let trace2 = [
+      {
+        values: aggYVals,
+        labels: xVals,
+        domain: { column: 0 },
+        hoverinfo: 'label+percent',
+        marker: {
+          line: {
+            color: 'white',
+            width: 1
+          }
+        },
+        textfont: {
+          family: 'Lato',
+          color: 'white',
+          size: 18
+        },
+        hoverlabel: {
+          bgcolor: 'black',
+          bordercolor: 'black',
+          font: {
+            family: 'Lato',
+            color: 'white',
+            size: 20
+          }
+        },
+        hole: .4,
+        type: "pie",
+      }
+    ];
+
+    // Create a layout for the donut chart 
+    let layout = {
+
+      title: {
+        text: 'Aggregate Crime Rate by Year and County',
+        font: {
+          size: 22,
+          family: 'Arial',
+          color: 'black'
+        }
+      },
+      // Specifies the text that goes inside the donut hole 
+      annotations: [
+        {
+          font: {
+            size: 20
+          },
+          showarrow: false,
+          text: 'Crime Rate',
+          textposition: 'inside',
+          x: 0.5,
+          y: 0.5
+        },
+
+      ],
+      height: 600,
+      width: 600,
+      showlegend: false,
+      grid: { rows: 1, columns: 1 }
+    };
+  
+    Plotly.newPlot("pie", trace2, layout);
+  };
+};
 
 init();
+
+
